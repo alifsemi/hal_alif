818455cb8ce86490b585420118bf1d13149d4254
Nisarga AM <nisarga.am@alifsemi.com>
2025-09-10 21:59:42 +0530

    drivers: add Analog HAL support
    Added Analog HAL support
    
    Signed-off-by: Nisarga AM <nisarga.am@alifsemi.com>

---
+++ drivers/analog/src/analog_ctrl.c
@@ -0,0 +1,19 @@
+/* Copyright (C) 2025 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ *
+ */
+
+#include <inttypes.h>
+
+#include "analog_ctrl.h"
+
+uint32_t analog_ldo_ref_cnt;
+uint32_t adc_vref_cnt;
+uint32_t dac6_vref_cnt;
+uint32_t dac12_vref_cnt;
+uint32_t cmp_clk_ref_cnt;
